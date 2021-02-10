ba43ac39c26b215a901126bf19176d203f15f9cf
RoitoLeonine <luoxiaowings@outlook.com>
2021-02-10 19:04:15 +0800

    Updated hardness.

--- src/main/java/cloud/lemonslice/barter/common/block/BlocksRegistry.java
+++ src/main/java/cloud/lemonslice/barter/common/block/BlocksRegistry.java
@@ -11,22 +11,22 @@ import static cloud.lemonslice.barter.Barter.ITEM_GROUP;
 
 public final class BlocksRegistry extends RegistryModule
 {
-    public static final Block WHITE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "white_trade_station");
-    public static final Block ORANGE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "orange_trade_station");
-    public static final Block MAGENTA_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "magenta_trade_station");
-    public static final Block LIGHT_BLUE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "light_blue_trade_station");
-    public static final Block YELLOW_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "yellow_trade_station");
-    public static final Block LIME_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "lime_trade_station");
-    public static final Block PINK_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "pink_trade_station");
-    public static final Block GRAY_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "gray_trade_station");
-    public static final Block LIGHT_GRAY_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "light_gray_trade_station");
-    public static final Block CYAN_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "cyan_trade_station");
-    public static final Block PURPLE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "purple_trade_station");
-    public static final Block BLUE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "blue_trade_station");
-    public static final Block BROWN_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "brown_trade_station");
-    public static final Block GREEN_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "green_trade_station");
-    public static final Block RED_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "red_trade_station");
-    public static final Block BLACK_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F), "black_trade_station");
+    public static final Block WHITE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "white_trade_station");
+    public static final Block ORANGE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "orange_trade_station");
+    public static final Block MAGENTA_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "magenta_trade_station");
+    public static final Block LIGHT_BLUE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "light_blue_trade_station");
+    public static final Block YELLOW_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "yellow_trade_station");
+    public static final Block LIME_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "lime_trade_station");
+    public static final Block PINK_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "pink_trade_station");
+    public static final Block GRAY_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "gray_trade_station");
+    public static final Block LIGHT_GRAY_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "light_gray_trade_station");
+    public static final Block CYAN_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "cyan_trade_station");
+    public static final Block PURPLE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "purple_trade_station");
+    public static final Block BLUE_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "blue_trade_station");
+    public static final Block BROWN_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "brown_trade_station");
+    public static final Block GREEN_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "green_trade_station");
+    public static final Block RED_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "red_trade_station");
+    public static final Block BLACK_TRADE_STATION = new NormalTradeStationBlock(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50.0F, 1200.0F), "black_trade_station");
 
     public static final BlockItem WHITE_TRADE_STATION_ITEM = new NormalBlockItem(WHITE_TRADE_STATION, ITEM_GROUP);
     public static final BlockItem ORANGE_TRADE_STATION_ITEM = new NormalBlockItem(ORANGE_TRADE_STATION, ITEM_GROUP);
